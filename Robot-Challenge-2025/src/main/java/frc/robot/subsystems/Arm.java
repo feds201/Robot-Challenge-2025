356132814ccced4eb1dd99f4fd611f72cbcbfdca
Clifford St. Clair <156970151+zombieking1555@users.noreply.github.com>
2025-11-13 20:19:00 -0500

    11/13 programming progress

--- Robot-Challenge-2025/src/main/java/frc/robot/subsystems/Arm.java
+++ Robot-Challenge-2025/src/main/java/frc/robot/subsystems/Arm.java
@@ -4,20 +4,73 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.LimelightThreeBase.LEDState;
 
 public class Arm extends SubsystemBase {
-  TalonFX motor = new TalonFX(8); //checking commit
-  TurretLimelight ll = new TurretLimelight("limelight-num", LEDState.OFF);
+
+
+  public enum armState{
+    //Straight out
+    ZERO(0),
+    TARGETING(90),
+    //Straight up
+    STOW(90);
+    private double angle;
+    armState(double angle){
+      this.angle = angle;
+    }
+    public double getAngle(){
+      return angle;
+    }
+  }
+  private armState state = null;
+  private TalonFX motor = new TalonFX(6); 
+  private CANcoder encoder = new CANcoder(7);
+  private TalonFXConfiguration tConfig = new TalonFXConfiguration();
+  private CANcoderConfiguration eConfig = new CANcoderConfiguration();
+  private TurretLimelight ll = new TurretLimelight("limelight-num", LEDState.OFF);
+
   /** Creates a new Arm. */
-  public Arm() {}
+  public Arm() {
+    eConfig.MagnetSensor.AbsoluteSensorDiscontinuityPoint = -.25;
+
+    tConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    tConfig.Slot0.kP = 0.0;
+    tConfig.Slot0.kD = 0.0;
+    tConfig.Slot0.kG = 0.0;
+    tConfig.Slot0.kS = 0.0;
+    tConfig.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+    tConfig.CurrentLimits.StatorCurrentLimit = 40; //40 Amp
+  
+
+  }
  
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+
+    switch(state){
+      case STOW:
+
+      break;
+      case TARGETING:
+
+      break;
+      case ZERO:
+
+      break;
+      default:
+
+      break;
+    }
+
   }
 
   
