03f2b7f0ba866bea80563ffdf224fc5316229812
Clifford St. Clair <156970151+zombieking1555@users.noreply.github.com>
2025-12-16 18:21:15 -0500

    added swerve and telemetry (drop in correct tunerconstants)

--- Robot-Challenge-2025/src/main/java/frc/robot/RobotContainer.java
+++ Robot-Challenge-2025/src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,27 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.TeleopSwerve;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.Arm.armState;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import frc.robot.subsystems.swerve.generated.TunerConstants;
 import frc.robot.subsystems.turret.TurretState;
 import frc.robot.subsystems.turret.TurretSubsystem;
 
 public class RobotContainer {
   private final Arm arm = new Arm();
   private final CommandXboxController controller = new CommandXboxController(0);
+  private CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+    private final Telemetry telemetry = new Telemetry(TunerConstants.kSpeedAt12Volts.in(MetersPerSecond));
    private TurretSubsystem turretSubsystem = new TurretSubsystem();
   public RobotContainer() {
     configureBindings();
+    drivetrain.registerTelemetry(telemetry::telemeterize);
   }
 
   private void configureBindings() {
@@ -21,7 +29,7 @@ public class RobotContainer {
     controller.b().onTrue(arm.commandState(armState.STOW));
     controller.y().onTrue(arm.commandState(armState.ZERO));
     controller.x().onTrue(turretSubsystem.setState(TurretState.Default));
-
+    drivetrain.setDefaultCommand(new TeleopSwerve(drivetrain, controller));
     }
 
     public Command getAutonomousCommand() {
